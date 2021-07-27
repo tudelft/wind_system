212e7a5c8c2c2fb2db9baf67fd04ec675458aa86
Raspberry Server <pi@tudelft.nl>
2021-07-27 16:02:58 +0200

    Server shows all RPM

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import time
 
+DEBUG = True
 FANDATA_FMT = "hhhhhhhhhh"
 
 fan_ip = [ '192.168.1.103', '192.168.1.104', '192.168.1.105']
@@ -13,26 +14,57 @@ def setfans(sock, number, data):
     target_ip = fan_ip[number]
 
     # Data
-    print('Fans ', number, ' -> ' ,target_ip, ': ', data)
+    if DEBUG:
+        print('Fans ', number, ' -> ' ,target_ip, ': ', data)
     
     # Sending
     try:
         ip_data = struct.pack(FANDATA_FMT, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9])
         sock.sendto(ip_data, (target_ip, target_port))
     except Exception as e:
-        print('Failed to send to',target_ip, e)
+        if DEBUG:
+            print('Failed to send to',target_ip, e)
 
 
 def getfans(sock):
     try:
         rcv_data, addr = sock.recvfrom(20)
-        rcv_count, rcv_val, val2, val3, val4, val5, val6, val7, val8, val9 = struct.unpack(FANDATA_FMT, rcv_data)
-        print('I received count ' + str(rcv_count) + ' and value ' + str(rcv_val) + "," + str(val3) + " from " + str(addr))
+        status, rpm1, rpm2, rpm3, rpm4, rpm5, rpm6, rpm7, rpm8, rpm9 = struct.unpack(FANDATA_FMT, rcv_data)
+        if DEBUG:
+            print('I received', status, rpm1, rpm2, rpm3, rpm4, rpm5, rpm6, rpm7, rpm8, rpm9,"from", addr)
         return True
     except Exception as e:
-        #print('No reply', e)
         return False
 
+ROWS = 9
+COLS = 15
+
+
+fans = [ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+         ]
+
+def fan_init( val ):
+    global fans
+    for x in range(0,COLS):
+        for y in range(0,ROWS):
+            fans[y][x] = val
+            
+fan_init(10)
+
+def fan_print(fans):
+    for r in fans:
+        print(r)
+ 
+
+fan_print(fans)
 
 def loop(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -45,10 +77,16 @@ def loop(port):
     while True:
         
         if time.time() > timetick:
-            timetick += 0.25
+            timetick += 2
             
             val = random.randint(0,100)
-            data = [count, val, 2,3,4,5,6,7,8,9]
+            data = [count, 400, 0, 400, 0, 1000, 0, 400 , 0, 400]
+            
+            data = [count, 400, 0, 400, 0, 1000, 0, 400 , 0, 400]
+            data = [count, 400, 0, 0, 0, 0, 0, 0 , 0, 0]
+            data = [count, 400, 400, 400, 400, 400, 400, 400 , 400, 400]
+            #data = [count, 800, 800, 800, 800, 800, 800, 800 , 800, 800]
+            #data = [count, val, 2,3,4,5,6,7,8,9]
             
             # Send to all modules
             i = 0
@@ -61,6 +99,8 @@ def loop(port):
         #time.sleep(1)
 
         
+        gotdata = getfans(sock)
+        gotdata = getfans(sock)
         gotdata = getfans(sock)
         
         
